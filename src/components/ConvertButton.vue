bebdaf52db39748d3bbfd371e49999c83fdcf559
kyushun <27068326+kyushun@users.noreply.github.com>
2019-11-23 15:03:08 +0900

    変換中は変換ボタンを押せないようにする

--- src/components/ConvertButton.vue
+++ src/components/ConvertButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="convert">
-    <div class="button" @click="onClick">
-      Convert
+    <div class="button" :class="{ disabled: !executable }" @click="onClick">
+      {{ executable ? "Convert" : "Converting..." }}
     </div>
   </div>
 </template>
@@ -13,12 +13,17 @@ import { CONVERT } from "../store/mutation-types";
 export default {
   name: "ConvertButton",
   computed: {
-    ...mapGetters(["sourceFilePath", "options", "convert"])
+    ...mapGetters(["sourceFilePath", "options", "convert"]),
+    executable: function() {
+      return !this.convert.status || this.convert.status == "finished";
+    }
   },
   methods: {
     ...mapActions([CONVERT]),
     onClick: function() {
-      this.CONVERT();
+      if (this.executable) {
+        this.CONVERT();
+      }
     }
   }
 };
@@ -38,6 +43,11 @@ export default {
 
     &:hover {
       background-color: #40a9ff;
+    }
+
+    &.disabled {
+      cursor: default;
+      background-color: #bae7ff;
     }
   }
 }
