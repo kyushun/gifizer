a48319cd15815d50f1ce26887a14e9551f6d169b
kyushun <27068326+kyushun@users.noreply.github.com>
2019-11-23 15:49:31 +0900

    ffmpeg がインストールされていない環境でも動くようにする

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,25 @@
+var os = require("os");
+var path = require("path");
+var platform = os.platform();
+
+const BIN_PATH = path.join(__dirname, "bin", platform, os.arch());
+const FFMPEG_PATH = path.join(
+  BIN_PATH,
+  platform === "win32" ? "ffmpeg.exe" : "ffmpeg"
+);
+const FFPROBE_PATH = path.join(
+  BIN_PATH,
+  platform === "win32" ? "ffprobe.exe" : "ffprobe"
+);
+
 module.exports = {
   pluginOptions: {
     electronBuilder: {
       chainWebpackMainProcess: config => {
         config.plugin("define").tap(args => {
           args[0]["process.env.FLUENTFFMPEG_COV"] = false;
+          args[0]["process.env.FFMPEG_PATH"] = JSON.stringify(FFMPEG_PATH);
+          args[0]["process.env.FFPROBE_PATH"] = JSON.stringify(FFPROBE_PATH);
           return args;
         });
       }
