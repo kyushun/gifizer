2748fd7b63f6135ca9956d4fe6a857bd5c094c19
kyushun <27068326+kyushun@users.noreply.github.com>
2021-03-06 13:49:59 +0900

    feat: show message text before input files

--- src/renderer/components/Edit/Edit.tsx
+++ src/renderer/components/Edit/Edit.tsx
@@ -1,9 +1,22 @@
+import { useRecoilValue } from 'recoil';
+
+import { inputFilePathState } from '@renderer/recoil/atoms';
+
 import { OptionContainer } from './OptionContainer';
+import { StyledFileInputAssistText } from './Styled';
 import { VideoPlayer } from './VideoPlayer';
 
-export const Edit = () => (
-  <>
-    <VideoPlayer />
-    <OptionContainer />
-  </>
-);
+export const Edit = () => {
+  const filePath = useRecoilValue(inputFilePathState);
+
+  return filePath ? (
+    <>
+      <VideoPlayer />
+      <OptionContainer />
+    </>
+  ) : (
+    <StyledFileInputAssistText>
+      Drag and drop your video
+    </StyledFileInputAssistText>
+  );
+};
