f6bd9b03f9cdd3f1796dd1646c08e0c9a3acfca0
kyushun <27068326+kyushun@users.noreply.github.com>
2021-05-01 00:56:59 +0900

    fix: broken ui on windows

--- src/renderer/components/Shared/Modal/Styled.tsx
+++ src/renderer/components/Shared/Modal/Styled.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
+import { isDarwin } from '@shared/util';
+
 export const Container = styled.div<{ isVisible: boolean }>`
   position: absolute;
-  top: 52px;
+  top: ${isDarwin ? '52px' : '82px'};
   left: 0;
   width: 100%;
-  height: calc(100% - 52px);
+  height: calc(100% - ${isDarwin ? '52px' : '82px'});
   background-color: rgba(0, 0, 0, 0.3);
   opacity: ${({ isVisible: visible }) => (visible ? '1' : '0')};
   backdrop-filter: ${({ theme }) => theme.palette.backdropBlur};
